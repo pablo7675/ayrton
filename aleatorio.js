76113ed53f59e70b9ca098622530b8280e13419f
pablo7675 <107186321+pablo7675@users.noreply.github.com>
2024-08-29 21:12:19 -0300

    Add files via upload

---
+++ aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Pablo", "Bryan",];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes);   
---
+++ perguntas.js
@@ -0,0 +1,152 @@
+export const perguntas = [
+    {
+        enunciado: "Qual era a equipe principal de Ayrton Senna durante sua carreira?",
+        alternativas: [
+            {
+                texto: "McLaren",
+                afirmacao: [
+                    "Senna conquistou três campeonatos mundiais com a McLaren, onde se destacou por suas habilidades excepcionais e rivalidades memoráveis.  .",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Williams ",
+                afirmacao: [
+                    "Ele correu pela Williams em seus últimos anos, mas é mais lembrado por suas conquistas na McLaren.",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Quantos campeonatos mundiais Ayrton Senna conquistou?.",
+        alternativas: [
+            {
+                texto: "3",
+                afirmacao: [
+                    "Ele venceu os títulos em 1988, 1990 e 1991, solidificando seu lugar entre os grandes da Fórmula ",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "4",
+                afirmacao: [
+                    "Esse número é associado a outros pilotos, como Alain Prost; Senna ficou com três.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Em que ano Ayrton Senna faleceu?",
+        alternativas: [
+            {
+                texto: "1994",
+                afirmacao: [
+                    "Sua morte ocorreu durante a corrida em Imola, um evento que mudou a história da Fórmula 1 em termos de segurança.  ",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " 2000",
+                afirmacao: [
+                    "Este ano não está relacionado à sua morte, mas sim ao legado que ele deixou.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual era uma das características mais marcantes da pilotagem de Senna?",
+        alternativas: [
+            {
+                texto: " Habilidade em condições molhadas  ",
+                afirmacao: [
+                    "Ele era excepcional em pistas molhadas, muitas vezes superando os adversários com sua técnica apurada.  ",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: " Estratégia conservadora",
+                afirmacao: [
+                    " Embora fosse agressivo, ele também sabia quando arriscar ou ser cauteloso durante as corridas."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Ayrton Senna era conhecido por seu envolvimento fora das pistas?",
+        alternativas: [
+            {
+                texto: " Sim, ele ajudou instituições de caridade  ",
+                afirmacao: [
+                    " Senna fundou o Instituto Ayrton Senna para ajudar crianças e jovens no Brasil, deixando um legado duradouro.  .",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não, ele era apenas focado nas corridas  ",
+                afirmacao: [
+                    "Apesar de sua dedicação ao automobilismo, seu impacto fora das pistas foi significativo e admirado..",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como é lembrado o legado de Ayrton Senna na Fórmula 1?",
+        alternativas: [
+            {
+                texto: " Como um ícone do esporte  ",
+                afirmacao: [
+                    "Ele é reverenciado como um dos maiores pilotos da história da Fórmula 1 e continua a inspirar muitos até hoje.  ",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: " Como um piloto controverso",
+                afirmacao: [
+                    " Embora tenha tido rivalidades intensas, seu talento e contribuições são amplamente reconhecidos e respeitados.",
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual era a relação de Ayrton Senna com Alain Prost?",
+        alternativas: [
+            {
+                texto: " Eram grandes amigos ",
+                afirmacao: [
+                    " Apesar de terem começado como amigos, a rivalidade entre eles se intensificou durante suas carreiras.  ",
+                ],
+                proxima: 8,
+            },
+            {
+                texto: "Eram rivais ferozes  ",
+                afirmacao: [
+                    "Senna e Prost tiveram uma das rivalidades mais famosas da Fórmula 1, com várias disputas intensas nas pistas.",
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual era a nacionalidade de Ayrton Senna?",
+        alternativas: [
+            {
+                texto: " Brasileiro  ",
+                afirmacao: [
+                    "Senna nasceu em São Paulo, Brasil, e se tornou um símbolo do automobilismo brasileiro.  ",
+                ],
+            },
+            {
+                texto: "Argentino  ",
+                afirmacao: [    
+                    "Ele não era argentino; sua identidade como brasileiro é uma parte fundamental de seu legado.",
+                ],
+            },
+        ]
+    }
+];
